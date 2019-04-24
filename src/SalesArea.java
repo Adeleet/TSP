cebf145e467a104a9450d1648886024a222e2368
Max Adaloudis <maxadaloudis@gmail.com>
2019-04-24 16:36:25 +0200

    Adds SalesArea class

---
+++ src/SalesArea.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class SalesArea {
+    private final double height;
+    private final double width;
+
+    private ArrayList<Position> destinations;
+    private Position position;
+
+    public SalesArea(double height, double width, Position initialPosition) {
+        this.position = initialPosition;
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void addDestination(Position d) {
+        destinations.add(d);
+    }
+
+    public ArrayList<Position> getDestinations() {
+        return destinations;
+    }
+
+}
